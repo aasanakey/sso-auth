bae1810bd7800131e685f50352b4b84e963a370a
Augustine Sanakey <sanakeyaugustineayiku@gmail.com>
2025-06-17 13:48:24 +0000

    Add utility function for masking character in text

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
+
+export const mask_text = (text:string,visibleChars:number= 4,mask = '*') => {
+  const masked = mask.repeat(text.length - visibleChars)
+  return masked + text.slice(-visibleChars)
+}
