56ee5bfcad8a3d19603de6be66fc56adeb580701
Augustine Sanakey <sanakeyaugustineayiku@gmail.com>
2025-06-25 17:02:20 +0000

    Create index file for exporting tables

---
+++ resources/js/components/tables/index.ts
@@ -0,0 +1,3 @@
+export { default as SocialProvidersTable } from "./SocialProvidersTable.vue";
+export { default as UserTable } from "./UserTable.vue";
+export { default as ClientTable } from "./ClientTable.vue";
