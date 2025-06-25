5dd87a429565968ca7131890d3d94a8ebc87d02d
Augustine Sanakey <sanakeyaugustineayiku@gmail.com>
2025-06-25 16:59:22 +0000

    Create page for manageing social account providers

---
+++ resources/js/pages/SocialProviders.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { SocialProviderModal } from '@/components/modals';
+import { SocialProvidersTable } from '@/components/tables';
+import AppLayout from '@/layouts/AppLayout.vue';
+import { Head } from '@inertiajs/vue3';
+
+</script>
+
+<template>
+
+    <Head title="Social Providers" />
+    <AppLayout>
+        <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
+            <div class="flex justify-end">
+                <SocialProviderModal />
+            </div>
+            <SocialProvidersTable />
+        </div>
+    </AppLayout>
+</template>
